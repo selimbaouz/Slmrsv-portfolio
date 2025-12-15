91dd60f08bb91dafbb4dbe3a9e4b54801dce36b0
Selim <slmrsv.bz@gmail.com>
2025-12-15 23:26:05 +0100

    New Gif

--- data/index.ts
+++ data/index.ts
@@ -4,83 +4,83 @@ export const templates = [
     imageUrl: "/images/palette_and_tints.png",
     url: "https://www.canva.com/your-apps/AAGluK9VFX0/palette-and-tints?q=Palette+and+Tints",
     category: "App Canva",
-    index: 0
+    index: 0,
   },
   {
     name: "HelloPurly",
     imageUrl: "/images/hellopurly-mobile.png",
     url: "",
     category: "Shopify Headless",
-    index: 1
+    index: 1,
   },
   {
     name: "MosqiBlock",
     imageUrl: "/images/MosqiBlock.png",
-    url: "https://mosqiblock.com",
+    url: "",
     category: "Shopify Headless",
-    index: 2
+    index: 2,
   },
   {
     name: "MosqiBlock",
     imageUrl: "/images/mosqiblock_product.png",
-    url: "https://mosqiblock.com",
+    url: "",
     category: "Shopify Headless",
-    index: 3
+    index: 3,
   },
   {
-    name: "TailwindLiquid",
-    imageUrl: "/images/Tw.png",
-    url: "https://tailwindliquid.com",
+    name: "EcomCodeHub",
+    imageUrl: "/images/ecomcodehub.png",
+    url: "https://ecomcodehub.com",
     category: "Shopify Headless",
-    index: 4
+    index: 4,
   },
   {
     name: "Maely",
     imageUrl: "/images/maely.png",
     url: "",
     category: "Design",
-    index: 5
+    index: 5,
   },
   {
     name: "Bright",
     imageUrl: "/images/bright-mode.png",
     url: "",
     category: "Design",
-    index: 6
+    index: 6,
   },
   {
     name: "Bright",
     imageUrl: "/images/bright-home.png",
     url: "",
     category: "Design",
-    index: 7
+    index: 7,
   },
   {
     name: "Maely",
     imageUrl: "/images/Maely - Header.png",
     url: "",
     category: "Design",
-    index: 8
+    index: 8,
   },
   {
     name: "Bagisty",
     imageUrl: "/images/bagisty.png",
     url: "",
     category: "Design",
-    index: 9
+    index: 9,
   },
   {
     name: "Luxurm",
     imageUrl: "/images/luxurm.png",
     url: "",
     category: "Design",
-    index: 10
+    index: 10,
   },
   {
     name: "WeMom",
     imageUrl: "/images/wemom.png",
     url: "",
     category: "Design",
-    index: 11
+    index: 11,
   },
 ];
